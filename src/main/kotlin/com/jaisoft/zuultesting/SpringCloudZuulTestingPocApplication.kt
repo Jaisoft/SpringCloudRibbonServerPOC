9415dd34aea2ebeb0f23bcc4dbc02f30219bc5de
Jaisoft <jaime.gomez.moraleda@gmail.com>
2020-03-30 21:53:17 +0200

    updated name main class

--- src/main/kotlin/com/jaisoft/zuultesting/SpringCloudZuulTestingPocApplication.kt
+++ src/main/kotlin/com/jaisoft/zuultesting/SpringCloudZuulTestingPocApplication.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class SpringCloudZuulTestingPocApplication
+class SpringCloudZuulServerPocApplication
 
 fun main(args: Array<String>) {
 	runApplication<SpringCloudZuulTestingPocApplication>(*args)
